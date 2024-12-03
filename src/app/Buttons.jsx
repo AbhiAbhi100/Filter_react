b0ab336eca35539ed0baf4b6f8b71e27157a1193
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2024-12-03 22:19:13 +0530

    added images

--- src/app/Buttons.jsx
+++ src/app/Buttons.jsx
@@ -1,20 +1,19 @@
+import React from 'react';
 
-
-
-
-import React from 'react'
-
-const Buttons = ({colors,filterItems,setData}) => {
+const Buttons = ({ colors, filterItems, setData }) => {
   return (
-    <div className='flex justify-center'>
-      {
-            colors.map((val,id)=>(
-                <button key={id} className='bg-red-500 text-white py-2 px-6 rounded-lg hover:bg-red-800 m-2' onClick={()=>filterItems(val)}>{val}</button>
-            ))
-      }
-
+    <div className="flex justify-center">
+      {colors.map((val, id) => (
+        <button
+          key={id}
+          className="bg-red-500 text-white py-2 px-6 rounded-lg hover:bg-red-800 m-2"
+          onClick={() => filterItems(val)}
+        >
+          {val}
+        </button>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
--- src/app/Card.jsx
+++ src/app/Card.jsx
@@ -1,23 +1,23 @@
+import React from 'react';
+import Img from './image.png';
+import Image from 'next/image';
 
-
-
-
-import React from 'react'
-
-const Card = ({index}) => {
+const Card = ({ index }) => {
   return (
-    <div >
-
-      <div className='text-center shadow-lg'>
-       <div className='overflow-hidden'>
-       <img src={index.url} className='hover:scale-125 duration-1000' />
-       </div>
-        <h3 className='font-bold text-2xl'>{index.title}</h3>
+    <div>
+      <div className="text-center shadow-lg">
+        <div className="overflow-hidden">
+          <Image
+            src={Img}
+            alt="img"
+            className="hover:scale-125 duration-1000"
+          />
+        </div>
+        <h3 className="font-bold text-2xl">{index.title}</h3>
         <p>Rs 8000</p>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
